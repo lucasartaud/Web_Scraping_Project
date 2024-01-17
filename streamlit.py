87e95957506c07d87f82cfe6e166711c2b4f944b
lucasartaud <lucas.artaud@gmail.com>
2024-01-17 09:04:48 +0100

    Update streamlit: filters work

--- streamlit.py
+++ streamlit.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-# Charger le DataFrame depuis le fichier CSV
 df = pd.read_csv('lunil.csv')
 
-# Titre de l'application
 st.markdown(
     """
     <style>
@@ -23,39 +21,41 @@ st.markdown(
 
 st.write("<h1 style='text-align: center; color:#3E9DF3; font-size: 80px;'>Projet Web Scraping</h1>", unsafe_allow_html=True)
 
-# Sidebar pour les filtres
 st.sidebar.header("Filtres")
 
-# Filtrer par marque
-marque_enabled = st.sidebar.checkbox("Activer le filtre par marque")
-marque = st.sidebar.selectbox("Filtrer par marque", ['Tesla', 'Renault', 'Mercedes'], key='marque') if marque_enabled else 'Tout'
-df_filtre_marque = df[df['Modèle'].str.contains(marque, case=False)] if marque != 'Tout' else df.copy()
+# Brand:
+enabled_brand = st.sidebar.checkbox("Activer le filtre par marque")
+brand = st.sidebar.selectbox("Filtrer par marque", ['Tesla', 'Renault', 'Mercedes', 'Peugeot', 'NIO'], key='marque') if enabled_brand else 'Tout'
+df_filtered_brand = df[df['Modèle'].str.contains(brand, case=False)] if brand != 'Tout' else df.copy()
 
-# Filtrer par prix
-prix_enabled = st.sidebar.checkbox("Activer le filtre par prix")
-prix = st.sidebar.slider("Filtrer par prix", float(df['Prix (euros) min'].min()), float(df['Prix (euros) max'].max()))  if prix_enabled else None
-df_filtre_prix = df[(df['Prix (euros) min'].isnull() | (df['Prix (euros) min'] <= prix)) & (df['Prix (euros) max'].isnull() | (df['Prix (euros) max'] >= prix))] if prix_enabled else df.copy()
+# Price:
+min_possible_price = float(df['Prix (euros) min'].min())
+max_possible_price = float(df['Prix (euros) max'].max())
+min_price, max_price = st.sidebar.slider("Filtrer par prix", min_possible_price, max_possible_price, (min_possible_price, max_possible_price))
+df_filtered_price = df.query(f'not (`Prix (euros) min` > {max_price} or `Prix (euros) max` < {min_price}) and not (`Prix (euros) min`.isnull() and `Prix (euros) max`.isnull())')
 
-# Filtrer par accélération
-acceleration_enabled = st.sidebar.checkbox("Activer le filtre par accélération")
-acceleration = st.sidebar.slider("Filtrer par accélération (0 à 100 km/h)", float(df['Accélération de 0 à 100 km/h (s) min'].min()), float(df['Accélération de 0 à 100 km/h (s) max'].max())) if acceleration_enabled else None
-df_filtre_acceleration = df[(df['Accélération de 0 à 100 km/h (s) min'].isnull() | (df['Accélération de 0 à 100 km/h (s) min'] <= acceleration)) & (df['Accélération de 0 à 100 km/h (s) max'].isnull() | (df['Accélération de 0 à 100 km/h (s) max'] >= acceleration))] if acceleration_enabled else df.copy()
+# Acceleration:
+min_possible_acceleration = float(df['Accélération de 0 à 100 km/h (s) min'].min())
+max_possible_acceleration = float(df['Accélération de 0 à 100 km/h (s) max'].max())
+min_acceleration, max_acceleration = st.sidebar.slider("Filtrer par accélération (0 à 100 km/h)", min_possible_acceleration, max_possible_acceleration, (min_possible_acceleration, max_possible_acceleration))
+df_filtered_acceleration = df.query(f'not (`Accélération de 0 à 100 km/h (s) min` > {max_acceleration} or `Accélération de 0 à 100 km/h (s) max` < {min_acceleration}) and not (`Accélération de 0 à 100 km/h (s) min`.isnull() and `Accélération de 0 à 100 km/h (s) max`.isnull())')
 
-# Filtrer par vitesse maximale
-vitesse_enabled = st.sidebar.checkbox("Activer le filtre par vitesse maximale")
-vitesse = st.sidebar.slider("Filtrer par vitesse maximale (km/h)", float(df['Vitesse maximale (km/h) min'].min()), float(df['Vitesse maximale (km/h) max'].max())) if vitesse_enabled else None
-df_filtre_vitesse = df[(df['Vitesse maximale (km/h) min'].isnull() | (df['Vitesse maximale (km/h) min'] <= vitesse)) & (df['Vitesse maximale (km/h) max'].isnull() | (df['Vitesse maximale (km/h) max'] >= vitesse))] if vitesse_enabled else df.copy()
+# Speed
+min_possible_speed = float(df['Vitesse maximale (km/h) min'].min())
+max_possible_speed = float(df['Vitesse maximale (km/h) max'].max())
+min_speed, max_speed = st.sidebar.slider("Filtrer par vitesse maximale (km/h)", min_possible_speed, max_possible_speed, (min_possible_speed, max_possible_speed))
+df_filtered_speed = df.query(f'not (`Vitesse maximale (km/h) min` > {max_speed} or `Vitesse maximale (km/h) max` < {min_speed}) and not (`Vitesse maximale (km/h) min`.isnull() and `Vitesse maximale (km/h) max`.isnull())')
 
-# st.write("df_filtre_marque")
-# st.dataframe(df_filtre_marque)
-# st.write("df_filtre_prix")
-# st.dataframe(df_filtre_prix)
-# st.write("df_filtre_acceleration")
-# st.dataframe(df_filtre_acceleration)
-# st.write("df_filtre_vitesse")
-# st.dataframe(df_filtre_vitesse)
+# st.write("df_filtered_brand")
+# st.dataframe(df_filtered_brand)
+# st.write("df_filtered_price")
+# st.dataframe(df_filtered_price)
+# st.write("df_filtered_acceleration")
+# st.dataframe(df_filtered_acceleration)
+# st.write("df_filtered_speed")
+# st.dataframe(df_filtered_speed)
 
-# Afficher le DataFrame filtré
-df_filtre_final = pd.merge(pd.merge(pd.merge(df_filtre_marque, df_filtre_prix, how='inner'), df_filtre_acceleration, how='inner'), df_filtre_vitesse, how='inner')
-# st.write("df_filtre_final")
-st.dataframe(df_filtre_final)
+df_filtered_final = pd.merge(pd.merge(pd.merge(df_filtered_brand, df_filtered_price, how='inner'), df_filtered_acceleration, how='inner'), df_filtered_speed, how='inner')
+
+# st.write("df_filtered_final")
+st.dataframe(df_filtered_final)
